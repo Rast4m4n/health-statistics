9318854973c5baaefb1659c3744cee9724d3d23e
Kirill <motovilink@yandex.ru>
2023-04-28 22:34:35 +0300

    Переписал логику сохранения пользовательских данных

--- lib/ui/pages/auth/auth_dialog_view_model.dart
+++ lib/ui/pages/auth/auth_dialog_view_model.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:health_statistics/data/api/services/health_api.dart';
+import 'package:health_statistics/data/api/services/user_api.dart';
+import 'package:health_statistics/data/repository/health_statistics_repository.dart';
 import 'package:health_statistics/data/storage/shared_preferencese.dart';
+import 'package:health_statistics/domain/auth.dart';
 import 'package:health_statistics/domain/enums/gender_enum.dart';
+import 'package:health_statistics/domain/models/user_model.dart';
 import 'package:health_statistics/ui/navigations/app_navigation.dart';
 
 class AuthDialogVIewModel {
@@ -10,9 +15,20 @@ class AuthDialogVIewModel {
   AuthDialogVIewModel({required this.textAge, required this.gender});
 
   Future<void> saveData(BuildContext context) async {
+    final accountGoogle = AuthGoogle();
+    final repo = HealthStatisticsRepository(
+      healthApi: HealthApi(),
+      userApi: UserApi(),
+    );
+    final users = await repo.fetchUserData();
+
     await SharedPrefRepository.instance.saveUserData(
-      gender!.nameGender,
-      int.parse(textAge.text),
+      UserModel(
+        id: users.isNotEmpty ? users.last.id + 1 : 1,
+        email: (await accountGoogle.getEmail())!,
+        gender: gender!.nameGender,
+        age: int.parse(textAge.text),
+      ),
     );
     if (context.mounted) Navigator.of(context).pushNamed(AppRoute.home);
   }
