66b872753be0bde10d709e850ad4ce1097b20755
Kirill <motovilink@yandex.ru>
2023-04-28 22:36:06 +0300

    Поменял название и назначение файла конфигураций api

---
+++ lib/data/api/config/config_api.dart
@@ -0,0 +1,12 @@
+abstract class ConfigApi {
+  // URL
+  // ip 10.0.2.2 для андроид эмулятора
+  static const _baseUri = 'http://10.0.2.2:8000';
+  static const healthUri = '$_baseUri/api/health/';
+  static const userUri = '$_baseUri/api/users/';
+
+  // headers
+  static const headersJson = <String, String>{
+    'Content-Type': 'application/json; charset=UTF-8',
+  };
+}
